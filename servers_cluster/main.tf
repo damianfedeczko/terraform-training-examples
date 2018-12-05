2511612fc42e85951f368c8ce26e22c65e4595af
damianfedeczko <damianfedeczko1@gmail.com>
2018-12-05 22:31:47 +0100

    Added the Auto Scaling Group and a Load Balancer

--- servers_cluster/main.tf
+++ servers_cluster/main.tf
@@ -1,3 +1,5 @@
+# Terraform plan to deploy an AWS EC2 server cluster with ELB load balancer
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -13,8 +15,8 @@ resource "aws_launch_configuration" "example" {
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
-  # the lifecycle can tell terraform to create a replacement
-  # for the destroyed resource like in the example below
+# the lifecycle block will tell terraform to create a replacement
+# for the destroyed resource like in the example below
   lifecycle {
     create_before_destroy = true
   }
@@ -34,11 +36,15 @@ resource "aws_security_group" "instance" {
     create_before_destroy = true
   }
 }
+
   data "aws_availability_zones" "all" {}
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 4
@@ -53,4 +59,53 @@ resource "aws_autoscaling_group" "example" {
 variable "server_port" {
   description = "This sets the port on the EC2 instance"
   default = 8080
+}
+
+# use a Elastic Load Balancer to distribute traffic accross server cluster
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+# health_check block sends an HTTP request every 30 seconds to the “/” URL
+# of each of the EC2 Instances in the ASG and only considers an Instance
+# healthy if it responds with a HTTP 200 status
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "elb_dns_name"{
+  value = "${aws_elb.example.dns_name}"
 }
